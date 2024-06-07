03f3c1d186b7652c3cf36fd7bad77b7f3de9bf1d
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-06-07 14:57:54 +0200

    feat: Finish exercises

--- src/test/java/java17/ex03/Function_03_Test.java
+++ src/test/java/java17/ex03/Function_03_Test.java
@@ -11,15 +11,18 @@ import java17.data.Person;
  */
 public class Function_03_Test {
 
-    //  tag::makeAChild[]
+    // tag::makeAChild[]
     // TODO Compléter la fonction makeAChild
     // TODO l'enfant possède le nom du père
     // TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
     // TODO l'age de l'enfant est 0
     // TODO le mot de passe de l'enfant est null
-    BinaryOperator<Person> makeAChild = null;
-    //  end::makeAChild[]
-
+    BinaryOperator<Person> makeAChild = (father, mother) -> new Person(
+            father.getFirstname() + " " + mother.getFirstname(),
+            father.getLastname(),
+            0,
+            null);
+    // end::makeAChild[]
 
     @Test
     public void test_makeAChild() throws Exception {
@@ -28,7 +31,7 @@ public class Function_03_Test {
         Person mother = new Person("Aline", "Lebreton", 22, "alino");
 
         // TODO compléter le test pour qu'il soit passant
-        Person child = null;
+        Person child = makeAChild.apply(father, mother);
 
         assert child.getFirstname().equals("John Aline");
         assert child.getLastname().equals("France");
