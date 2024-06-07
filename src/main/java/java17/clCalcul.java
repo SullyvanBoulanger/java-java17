59cc3a1d48b0df82da79dbb5cfc30f3af5940419
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-06-07 11:31:59 +0200

    feat: add substract

--- src/main/java/java17/clCalcul.java
+++ src/main/java/java17/clCalcul.java
@@ -1,19 +1,18 @@
 package java17;
 
-
 public class clCalcul {
 
 	public Double exec(Double a, Double b, iOperation operation) {
 		return operation.apply(a, b);
 	}
-	
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		clCalcul calcul = new clCalcul();
-		iOperation addition = (c,d) -> c + d;
-		
+		iOperation addition = (c, d) -> c + d;
+		iOperation soustraction = (c, d) -> c - d;
+
 		System.out.println(calcul.exec(120.10, 25.20, addition));
-		
+		System.out.println(calcul.exec(80.20, 25.20, soustraction));
 	}
 
 }
--- src/main/java/java17/iOperation.java
+++ src/main/java/java17/iOperation.java
@@ -2,5 +2,8 @@ package java17;
 
 public interface iOperation {
 	public Double apply(Double a, Double b);
-	
+	/**
+	 * On ne peut pas avoir une autre méthode dans une "interface fonctionnelle : Functionnal Interface"
+	 * public Double apply01(Double a, Double b);
+	 */
 }
